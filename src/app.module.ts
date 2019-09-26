d3926ccfdd30c9f56c6a7e73da445adf278261a9
miZye <mi.zyeman@gmail.com>
2019-09-26 14:55:38 +0200

    Added view engine pug Added product and order as controllers and models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
+import { IndexController } from './controllers/index.controller';
+
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    IndexController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
