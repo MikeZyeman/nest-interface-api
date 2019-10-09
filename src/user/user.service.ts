18eafbdbd5d49c5fb50a7adefdd4039b17a5b118
miZye <mi.zyeman@gmail.com>
2019-10-09 18:37:30 +0200

    implementing authentication module

---
+++ src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UserService {
+
+  private readonly users: User[] = [];
+
+  constructor() {
+
+  }
+
+
+
+}
